3f55d99a31e50a1dbe3cedca94a67c78612ec8f5
Jock Reed <jock.da.rock@gmail.com>
2019-06-09 12:24:48 -0700

    Update mqtt_pub.py

--- mqtt/mqtt_pub.py
+++ mqtt/mqtt_pub.py
@@ -6,23 +6,22 @@ broker_port = 1883
 mqtt_topic = 'devnetzone/topic'
 payload = '{"message":"Hello World"}'
 
-def on_connect(mqttc, obj, flags, rc):
-    print("connected")
+def on_connect(client, obj, flags, rc):
+    # self.master.subscribe(mqtt_topic)
+    print("connected" + str(rc))
 
 
-def on_publish(mqttc, obj, mid):
+def on_publish(client, obj, mid):
     print("mid: " + str(mid))
-    pass
 
-def mqtt_pub(payload): #function to publish payload to a topic
-    client = mqtt.Client()
+def mqtt_pub(client, payload): #function to publish payload to a topic
     client.on_connect = on_connect
     client.on_publish = on_publish
     client.connect(broker_ip, broker_port, 60)
     client.publish(mqtt_topic, payload)
     print('MESSAGE PUB [x] ' + payload)
-    client.disconnect()
 
 
 if __name__ == '__main__':
-    mqtt_pub(payload)
+    client = mqtt.Client()
+    mqtt_pub(client, payload)
